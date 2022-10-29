7bfff4bf7684d72eb7b16eac90a8462b6a3aeef2
Pedro Almodovar <pedro.pinto@contabilizei.com.br>
2022-10-29 16:25:17 -0300

    documentation: Ajustes em swagger

--- src/main/java/com/pedro/accountmanager/config/SwaggerConfig.java
+++ src/main/java/com/pedro/accountmanager/config/SwaggerConfig.java
@@ -2,21 +2,32 @@ package com.pedro.accountmanager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
+import springfox.documentation.service.Response;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public Docket api() {
+        List<Response> responses = new ArrayList<>();
+        responses.add(new ResponseBuilder().code("400").description("Bad Request").build());
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.pedro.accountmanager.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .useDefaultResponseMessages(false)
+                .globalResponses(HttpMethod.GET, responses);
     }
 }
